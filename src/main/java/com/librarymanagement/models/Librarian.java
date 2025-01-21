b41502edcf9e02ae1a89d6067444017fbd812891
Adedayo-Data <theophilusdayo@outlook.com>
2025-01-21 11:29:35 +0100

    Librarian class added

--- src/main/java/com/librarymanagement/models/Librarian.java
+++ src/main/java/com/librarymanagement/models/Librarian.java
@@ -1,1 +1,15 @@
-package com.
+package com.librarymanagement.models;
+
+public class Librarian extends User{
+
+    protected Long Id;
+
+    Librarian(String name, Role role, String password, Long Id){
+        super(name, name, null, password);
+        this.Id = Id;
+    }
+
+    public void addUser(User user){
+        
+    }
+}
